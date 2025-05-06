ddcff489467f4e95a4866fefde5e55b4b6332dfe
e.zheltukhin <e.zheltukhin@kt-team.de>
2025-05-06 22:34:08 +0500

    Enable cors for dev

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    exposedHeaders: 'set-cookie'
+  })
+
   await app.listen(process.env.PORT ?? 4200);
 }
 
